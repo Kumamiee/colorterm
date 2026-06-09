7c65774ff433b1da02bd96b58240c36c7cd18a9c
Kumamiee <k4239054@gmail.com>
2026-06-09 10:49:47 +0900

    library

---
+++ colorterm.hpp
@@ -0,0 +1,23 @@
+/* 
+* Copyright (c) 2026 Kumamiee
+* LICENSE "MIT"
+* Dev by Kumamiee
+* Requirement: C++98 or later
+*/
+#ifndef _COLOR_UTIL_KUMAMIEE_HPP_
+#define _COLOR_UTIL_KUMAMIEE_HPP_
+
+
+namespace Kumalib {
+    namespace color {
+    static const char* const cyan = "\033[36m";
+    static const char* const red = "\033[31m";
+    static const char* const reset = "\033[m";
+    static const char* const blue = "\033[34m";
+    static const char* const black = "\033[30m";
+    static const char* const green = "\033[32m";
+    static const char* const yellow = "\033[33m";
+    static const char* const magenta = "\033[35m";
+}
+}
+#endif
